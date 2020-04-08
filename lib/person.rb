0117058bcd3bc8d92687ee739f717f6cc4beff99
Christopher <reinkendigital@gmail.com>
2020-04-07 19:36:39 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,15 @@
 class Person
-  #your code here
+
+    #### Attributes ####
+    attr_accessor :name, :birthday, :hair_color, :eye_color, :height,
+    :weight, :handed, :complexion, :t_shirt_size, :wrist_size,
+    :glove_size, :pant_length, :pant_width
+    #### Instance Methods ####
+    def initialize(attributes)
+        attributes.each {|key, value| self.send(("#{key}="), value)}
+
+    end
+
+    #### Class Methods####
+
 end
